c3eb57560e5da8ae83ed4bdc32690bd88a8ce4da
Taroryo <107976321+Taroryo@users.noreply.github.com>
2022-07-11 17:24:51 +0900

    Update 03.cpp
    3番の解答を公開
    for文を簡潔に書けるようにしてみた

--- 03.cpp
+++ 03.cpp
@@ -1,1 +1,43 @@
-
+#include<stdio.h>
+#define roop(i,a,b) for(int i=a;i<b;i++)
+int main(void)
+{
+    int x,y;
+    const char *poin = "input.pgm";
+    const char *pout = "output.pgm";
+    FILE *input_file = fopen(poin, "r");
+    FILE *output_file = fopen(pout, "w");
+    if (input_file == NULL)
+    {
+        printf("Error opening file %s\n", poin);
+        return 1;
+    }
+    if (output_file == NULL)
+    {
+        printf("Error opening file %s\n", pout);
+        return 1;
+    }
+    int din[10][10],dout[10][10];
+    roop(x,0,10)
+    {
+        roop(y,0,10)
+        {
+            fscanf(input_file,"%d ",&din[x][y]);
+        }
+    }
+    roop(x,0,10)
+    {
+        roop(y,0,10)
+        {
+            dout[10-x-1][10-y-1] = din[x][y];
+        }
+        fprintf(input_file, "\n");
+    }
+    roop(x,0,10)
+    {
+        roop(y,0,10){ fprintf(output_file, "%d ", dout[x][y]);}
+        fprintf(output_file, "\n");
+    }
+    fclose(input_file); fclose(output_file);
+    return 0;
+}
